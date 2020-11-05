b4adf637d222d4dfb86fd9803d675097e519c993
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-11-05 14:15:48 +0100

    Updates

---
+++ precache-manifest.780cb92805dcf8544fbb41b2672febb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395c625238d697b70c04e2a7397e1b66",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "4852ff37187fc05369a9",
+    "url": "/pomodoro/static/css/main.a0296c49.chunk.css"
+  },
+  {
+    "revision": "cbf6c488f252dd39af10",
+    "url": "/pomodoro/static/js/2.790b3750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.790b3750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4852ff37187fc05369a9",
+    "url": "/pomodoro/static/js/main.5151d0ab.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
